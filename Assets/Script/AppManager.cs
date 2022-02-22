637896b1bcabe2fae4a2995b2286c5efb42425e0
gem_yours <urgems000@gmail.com>
2022-02-22 13:37:07 +0900

    [add] create tab button

---
+++ Assets/Script/AppManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppManager : MonoBehaviour
+{
+    public static AppManager instance;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else if (instance == this) {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
