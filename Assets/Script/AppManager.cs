267ed717f4ffac82ffa6f66a794f968b2a15dd3c
gem_yours <urgems000@gmail.com>
2022-02-23 17:40:25 +0900

    [create] enable switch tab

--- Assets/Script/AppManager.cs
+++ Assets/Script/AppManager.cs
@@ -2,24 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AppManager : MonoBehaviour
 {
     public static AppManager instance;
 
-    private void Awake() {
-        if (instance == null) {
+    private Button battleButton;
+    private Button equipButton;
+    private GameObject battleScreen;
+    private GameObject equipScreen;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
             instance = this;
             DontDestroyOnLoad(gameObject);
-        } else if (instance == this) {
+        } else if (instance == this)
+        {
             Destroy(gameObject);
         }
+    }
+
+
+    private void Init() 
+    {
+        battleButton = GameObject.Find("BattleButton").GetComponent<Button>();
+        equipButton = GameObject.Find("EquipButton").GetComponent<Button>();
+        battleScreen = GameObject.Find("BattleScreen");
+        equipScreen = GameObject.Find("EquipScreen");
+
+        
+        battleButton.onClick.AddListener(() => ActivateScreen(battleScreen));
+        equipButton.onClick.AddListener(() => ActivateScreen(equipScreen));
+
+        ActivateScreen(battleScreen);
+    }
+
+    private void ActivateScreen(GameObject screen)
+    {
+        battleScreen.SetActive(false);
+        equipScreen.SetActive(false);
+
+        screen.SetActive(true);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
