ce56b85a51b88ba6c7b3805c1223cf1d9dbc74f0
gem_yours <urgems000@gmail.com>
2022-04-11 11:58:05 +0900

    [add] add test

--- Assets/Script/Player/Tests/DeckTest.cs
+++ Assets/Script/Player/Tests/DeckTest.cs
@@ -1,49 +1,48 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using NUnit.Framework;
-// using UnityEngine;
-// using UnityEngine.TestTools;
-// using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
 
-// #nullable enable
-// public class DeckTest
-// {
-// #pragma warning disable CS8618
-//     private Deck deck;
-//     private List<Spell> spells;
-// #pragma warning restore CS8618
+#nullable enable
+public class DeckTest
+{
+#pragma warning disable CS8618
+    private Deck deck;
+    private List<Spell> spells;
+#pragma warning restore CS8618
 
-//     [OneTimeSetup]
-//     public void OneTimeSetup()
-//     {
-//         spells = new List<Spell> {
-//             new Ignis(),
-//             new Explosion(),
-//             new Ignis()
-//         };
-//         deck = new Deck(spells, 0, 0);
-//     }
+    [SetUp]
+    public void OneTimeSetup()
+    {
+        spells = new List<Spell> {
+            new Ignis(),
+            new Explosion(),
+            new Ignis()
+        };
+        deck = new Deck(spells, 0, 0);
+    }
 
-//     // A Test behaves as an ordinary method
-//     [Test]
-//     public void CheckDrawPile()
-//     {
-//         Assert.isEqual(deck.drawPile, spells);
-//     }
+    // A Test behaves as an ordinary method
+    [Test]
+    public void CheckDrawPile()
+    {
+        Assert.AreEqual(deck.drawPile, spells);
+    }
 
-//     [Test]
-//     public void CheckDiscardPile()
-//     {
+    [Test]
+    public void CheckDiscardPile()
+    {
 
-//     }
+    }
 
-//     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-//     // `yield return null;` to skip a frame.
-//     [UnityTest]
-//     public IEnumerator DeckTestWithEnumeratorPasses()
-//     {
-//         // Use the Assert class to test conditions.
-//         // Use yield to skip a frame.
-//         yield return null;
-//     }
-// }
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator DeckTestWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
