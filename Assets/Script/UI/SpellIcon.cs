13e8adf176df977860a8a4a563e9b26146f21396
gem_yours <urgems000@gmail.com>
2022-05-20 18:25:46 +0900

    [add] remove background at drawed spell #35

--- Assets/Script/UI/SpellIcon.cs
+++ Assets/Script/UI/SpellIcon.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 #nullable enable
 public class SpellIcon : MonoBehaviour
 {
-    public Image? spellImage;
-    private Rigidbody2D? rb2D;
-
+#pragma warning disable CS8618
+    public Image spellImage;
+#pragma warning restore CS8618
     private Spell? _spell;
     public Spell? spell
     {
@@ -23,6 +23,10 @@ public class SpellIcon : MonoBehaviour
             return _spell;
         }
     }
+#pragma warning disable CS8618
+    private Rigidbody2D rb2D;
+#pragma warning restore CS8618
+
 
     public void AttachTo(GameObject target)
     {
@@ -35,26 +39,47 @@ public class SpellIcon : MonoBehaviour
 
     private IEnumerator _Attach(GameObject target)
     {
+        DeleteBackground();
         var animationDuration = 0.5f;
+        var rectTransform = GetComponent<RectTransform>();
+        var targetRectTransform = target.GetComponent<RectTransform>();
+        // targetはsizefitterで幅を調節している
+        // 知識が漏れてる感じはするが、現状いい解決策が思いつかないのでこれで対応する
+        var widthScale = (rectTransform is not null && targetRectTransform is not null) ?
+#pragma warning disable CS8602
+        targetRectTransform.sizeDelta.x / rectTransform.sizeDelta.x : 1f;
+        var initialWidth = rectTransform.sizeDelta.x;
+#pragma warning restore CS8602
+
         yield return AnimationUtil.EaseInOut(
             animationDuration,
             (current) =>
             {
                 var displacment = transform.position - target.transform.position;
                 transform.position = target.transform.position + displacment * current;
+                transform.localScale = new Vector2(
+                    1 + (widthScale - 1) * current,
+                    1 + (widthScale - 1) * current
+                );
+                Debug.Log(spellImage.transform.localScale);
             }
         );
+        transform.position = target.transform.position;
+    }
+
+
+    private void DeleteBackground()
+    {
+        var image = GetComponent<Image>();
+        if (image is not null)
+        {
+            Destroy(image);
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
